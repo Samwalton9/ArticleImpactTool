0365d0978d2858b3fc0db6549ebf17e42f7070c2
Sam Walton <samwalton9@gmail.com>
2021-08-21 12:58:03 +0100

    Fix form, send results to result page

--- app.py
+++ app.py
@@ -1,14 +1,33 @@
-from flask import Flask, render_template, request
+from yaml import safe_load
+
+from flask import Flask, render_template, request, redirect, url_for
+from flask_wtf import FlaskForm
+from wtforms import StringField, validators
 
 app = Flask(__name__)
 
+with open('secrets.yml', 'r') as secrets_file:
+    secrets = safe_load(secrets_file)
+
+app.config['SECRET_KEY'] = secrets['secret_key']
+
 
 @app.route('/', methods=['GET', 'POST'])
-def homepage():  # put application's code here
+def homepage():
     if request.method == 'POST':
-        pass
+        language = request.form.get('language')
+        article_title = request.form.get('title')
+        query_string = f"?lang={language}&title={article_title}"
+        return redirect(url_for('results_page') + query_string)
     else:
         return render_template('homepage.html')
+
+
+@app.route('/result', methods=['GET', 'POST'])
+def results_page():
+    language = request.args.get('lang')
+    title = request.args.get('title')
+    return render_template('result.html', language=language, title=title)
 
 
 if __name__ == '__main__':
